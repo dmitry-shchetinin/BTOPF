d4bfa0c1c80dc2fcf5aafad65812966ed1c28293
sdmitry <sdmitry@ict-networks-192-168-099-001.fwd-v4.ethz.ch>
2018-08-21 10:58:03 +0200

    added KLU option to BT through power flows

--- matlab/BTtighten_through_flows.m
+++ matlab/BTtighten_through_flows.m
@@ -3,15 +3,21 @@ function [ bounds ] = BTtighten_through_flows( bus, branch, Env, opt, tighten_an
 %theta (if tighten_angle=1) or Vdif (if tighten_angle=0) for all branches.
     
 %unpack data
-[ind_bus_F, ind_bus_T, L, N, opt_prob_flow, use_mex, ind_ref, load_PQ, ind_gen]=...
-    deal(branch.ind_bus_F, branch.ind_bus_T, branch.nbranch, bus.nbus, ...
-    opt.opt_prob_flow, opt.use_mex, bus.ind_ref, bus.load_PQ, bus.ind_gen_PQ);
+[ind_bus_F, ind_bus_T, L, N, opt_prob_flow, use_mex, ind_ref, load_PQ, ind_gen, linsolver]=...
+    deal(branch.ind_bus_F, branch.ind_bus_T, branch.nbranch, bus.nbus, opt.opt_prob_flow, ...
+    opt.use_mex, bus.ind_ref, bus.load_PQ, bus.ind_gen_PQ, opt.linsolver);
     
 %extract part of A related to slack variables (envelopes' wiggle room)
 A_rhs=-transpose(Env.A(:,2*N:end)); %we will only need transpose
 
 %factorize transpose of part of A related to V and theta
-[LL, UU, p, q] = lu(transpose(Env.A(:,1:2*N-1)),'vector');    %Alhs(q,p) = L_lhs*U_lhs
+if (linsolver==1)
+    [LL, UU, p, q] = lu(transpose(Env.A(:,1:2*N-1)),'vector');    %Alhs(q,p) = L_lhs*U_lhs
+    LU_struct=[];
+else
+    LU_struct=klu(transpose(Env.A(:,1:2*N-1)));
+    LL=[]; UU=[]; p=[]; q=[];
+end
 
 %record bounds on vector of variables
 x_min=[bus.Pmin(bus.ind_genbus); bus.Qmin(bus.ind_genbus); Env.bl];
@@ -37,27 +43,39 @@ parfor i=1:L
     i_end=ind_bus_T(i);
     
     %compute rhs vector for obtaining required row of inverse
+    ind_1=0; ind_2=0;
     if (tighten_angle)
         %get row of inverse corresponding to theta_ij
-        e_vector=zeros(2*N-1,1);
         if (i_begin<ind_ref)
-            e_vector(p==N+i_begin)=1;
+            ind_1=N+i_begin;
         elseif (i_begin>ind_ref)
-            e_vector(p==N+i_begin-1)=1;
+            ind_1=N+i_begin-1;
         end
         if (i_end<ind_ref)
-            e_vector(p==N+i_end)=-1;
+            ind_2=N+i_end;
         elseif (i_end>ind_ref)
-            e_vector(p==N+i_end-1)=-1;
+            ind_2=N+i_end-1;
         end
     else
         %get row of inverse corresponding to Vi-Vj
-        e_vector=zeros(2*N-1,1);
-        e_vector(p==i_begin)=1;
-        e_vector(p==i_end)=-1;
+        ind_1=i_begin;
+        ind_2=i_end;
     end
-    row_bound=UU\(LL\e_vector);
-    row_bound(q)=row_bound;
+    e_vector=zeros(2*N-1,1);
+    if (linsolver==1)
+        e_vector(p==ind_1)=1;
+        e_vector(p==ind_2)=-1;
+        row_bound=UU\(LL\e_vector);
+        row_bound(q)=row_bound;
+    else
+        if (ind_1~=0)
+            e_vector(ind_1)=1;
+        end
+        if (ind_2~=0)
+            e_vector(ind_2)=-1;
+        end
+        row_bound=klu(LU_struct,'\',e_vector);
+    end
     
     %get contribution from loads to objective
     objconst=load_PQ*row_bound;
@@ -73,9 +91,14 @@ parfor i=1:L
     else
         %solve LP with one constraint
         e_vector=zeros(2*N-1,1);
-        e_vector(p==i_begin)=1;
-        row_Vi=UU\(LL\e_vector);
-        row_Vi(q)=row_Vi;
+        if (linsolver==1)
+            e_vector(p==i_begin)=1;
+            row_Vi=UU\(LL\e_vector);
+            row_Vi(q)=row_Vi;
+        else
+           e_vector(i_begin)=1;
+           row_Vi=klu(LU_struct,'\',e_vector);
+        end
         %get contribution from loads to objective
         b_const=load_PQ*row_Vi;
         c=c';
